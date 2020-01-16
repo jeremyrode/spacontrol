314bebeacea341ba9368dc7c95d21c57b289f444
Jeremy Rode <jprode@gmail.com>
2020-01-16 14:53:03 -0800

    Inital testing and frontend work

--- tools/newpcb_sendjets.js
+++ tools/newpcb_sendjets.js
@@ -4,16 +4,20 @@ const Delimiter = require('@serialport/parser-delimiter');
 var Gpio = require('onoff').Gpio; //require onoff to control GPIO
 var MUXPin = new Gpio(5, 'out'); //declare GPIO5 an output
 
-const long_pattern = Buffer.alloc(6,'036b00000166','hex');
-const short_pattern = Buffer.alloc(4,'014b0144', 'hex');
-const jets_pattern = Buffer.alloc(10 ,'584D5300036b00100176', 'hex');
+const header =        Buffer.alloc(4  ,'584D5300','hex'); //Header for every command
+const long_pattern =  Buffer.alloc(6  ,'036b00000166','hex'); //No button pressed pattern
+const short_pattern = Buffer.alloc(4  ,'014b0144', 'hex'); //Unsure but it perodically appears
+const jets_pattern =  Buffer.alloc(10 ,'584D5300036b00100176', 'hex'); //jets button
+const temp_up =       Buffer.alloc(10 ,'584D5300036b0008016e', 'hex');
+const temp_down =     Buffer.alloc(10 ,'584D5300036b00020168', 'hex');
+const lights =        Buffer.alloc(10 ,'584D5300036b00200186', 'hex');
 
 
 const port = new SerialPort('/dev/serial0', {
   baudRate: 115200
 })
 
-const parser = port.pipe(new Delimiter({delimiter: new Buffer('584D5300','hex') }));
+const parser = port.pipe(new Delimiter({delimiter: header }));
 parser.on('data', function(data) {onDiffData(data);});
 
 function onDiffData(sdata) {
@@ -28,18 +32,9 @@ setInterval(sendJets,10000);
 
 function sendJets() {
   console.log('Sending Jets');
-  mux_on();
-  setTimeout(sendJetsPat,100);
-  setTimeout(sendJetsPat,140);
-  setTimeout(sendJetsPat,180);
-  setTimeout(sendJetsPat,220);
-  setTimeout(sendJetsPat,260);
-  setTimeout(sendJetsPat,300);
-  setTimeout(sendJetsPat,340);
-  setTimeout(sendJetsPat,380);
-  setTimeout(sendJetsPat,420);
-  setTimeout(sendJetsPat,480);
-  setTimeout(mux_off,500);
+  mux_on(); // Take control of the serial bus
+  setTimeout(sendJetsPat,40); //sent the command 40 ms later
+  setTimeout(mux_off,80); //Give back control of the bus
 	console.log('Sent Jets');
 }
 

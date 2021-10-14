bfe07bdb1d4eccd4a1c3056a263686059936e1d3
Jeremy Rode <jprode@gmail.com>
2021-10-13 17:32:46 -0700

    Adapting tools for screen to CPU commands

--- tools/serial_test_screen.py
+++ tools/serial_test_screen.py
@@ -5,10 +5,10 @@ import serial
 ser = serial.Serial(
     port='/dev/serial0',
     baudrate = 115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,#35B (minus 6 bits) with two stop bits 
-    bytesize=serial.EIGHTBITS,  # 38 bytes with one stop bit
-    timeout=0.1
+#    parity=serial.PARITY_NONE,
+#    stopbits=serial.STOPBITS_ONE,#35B (minus 6 bits) with two stop bits
+#    bytesize=serial.EIGHTBITS,  # 38 bytes with one stop bit
+    #timeout=0.05,
     )
 old=ser.read(10)
 #old_bin_string = bin(int(old.hex(),16))
@@ -16,6 +16,6 @@ print(old.hex())
 while True:
     data=ser.read(10)
     if (data != old):
-        bin_string = bin(int(data.hex(),16))
+        #bin_string = bin(int(data.hex(),16))
         print(data.hex())
         old = data
